42f446347a8837aca90244064ee4322f5b903083
pavel.genuine <pavel.play@gmail.com>
2022-07-01 07:07:21 +0600

    expressjs config

---
+++ Index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const secretTicket = 'shhh'
+require('dotenv').config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const port = process.env.PORT || 5000
+const app = express();
+
+
+//middlewire
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.pbmjp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
